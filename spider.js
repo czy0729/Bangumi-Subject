12a8bdacebe0153a55248ad3b17224fe5b2c7877
czy0729 <402731062@qq.com>
2020-08-09 18:12:28 +0800

    update 2020/08/09

--- spider.js
+++ spider.js
@@ -2,22 +2,23 @@
  * @Author: czy0729
  * @Date: 2020-01-14 18:51:27
  * @Last Modified by: czy0729
- * @Last Modified time: 2020-08-03 21:46:06
+ * @Last Modified time: 2020-08-09 05:41:39
  */
 const axios = require('axios')
 const fs = require('fs')
 const path = require('path')
 const cheerio = require('./utils/cheerio')
 const utils = require('./utils/utils')
+
 axios.defaults.timeout = 3000
 
 const host = 'bgm.tv'
 const rewrite = false
 const startIndex = 0
-const queue = 6
+const queue = 8
 const ids = [
-  ...JSON.parse(fs.readFileSync('./ids/anime-2020.json')),
   ...JSON.parse(fs.readFileSync('./ids/anime-2021.json')),
+  ...JSON.parse(fs.readFileSync('./ids/anime-2020.json')),
   ...JSON.parse(fs.readFileSync('./ids/anime-rank.json')),
   ...JSON.parse(fs.readFileSync('./ids/anime-bangumi-data.json')),
   ...JSON.parse(fs.readFileSync('./ids/book-rank.json')),
@@ -34,9 +35,9 @@ JSON.stringify({
 */
 const headers = {
   'User-Agent':
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
   Cookie:
-    'chii_cookietime=2592000; chii_theme_choose=1; prg_list_mode=full; prg_display_mode=normal; __utmz=1.1591106515.1329.64.utmcsr=tongji.baidu.com|utmccn=(referral)|utmcmd=referral|utmcct=/web/28208841/trend/latest; chii_theme=dark; __utmc=1; chii_searchDateLine=0; __utma=1.7292625.1567003648.1592078520.1592111345.1374; __utmt=1; chii_sid=Fuk89I; chii_auth=L6%2FeXtZPnDHiE67klhtmJt7Rs0U9mZeJNZkaH4yQeUyop9ex6CiML4oXrEB%2Fpp%2FNR%2BFkPVBece6mH%2FMbwrnoYg4fFbratYJzWE%2B7; __utmb=1.5.10.1592111345',
+    '__cfduid=d10ce460503307836b7f7dfc6f19b10b11567003647; chii_cookietime=2592000; chii_theme_choose=1; prg_list_mode=full; prg_display_mode=normal; __utmz=1.1595138713.1545.69.utmcsr=tongji.baidu.com|utmccn=(referral)|utmcmd=referral|utmcct=/web/28208841/trend/latest; chii_theme=dark; __utmc=1; chii_searchDateLine=0; __utma=1.7292625.1567003648.1596860087.1596865033.1622; __utmt=1; chii_sid=zk53HA; chii_auth=MhC3h6SUV9MTRltnbl0U2HMA9swTI%2BN0tdxPtvPS9QKYgfQcIxdcgZzHrX44UF4JWEST2xQTCEdtMFkFBpV9yv8BZAAW824QDdZD; __utmb=1.4.10.1596865033',
 }
 
 async function fetchSubject(id, index) {
