2fd078e2477d03e853ed45ad8cb0fd05f182d295
czy0729 <402731062@qq.com>
2021-04-17 14:27:32 +0800

    update

--- ids.js
+++ ids.js
@@ -3,7 +3,7 @@
  * @Author: czy0729
  * @Date: 2020-01-15 10:17:38
  * @Last Modified by: czy0729
- * @Last Modified time: 2021-03-09 14:40:39
+ * @Last Modified time: 2021-04-17 01:47:16
  */
 const axios = require('axios')
 const fs = require('fs')
@@ -44,8 +44,8 @@ async function fetch(url) {
  *  - https://bgm.tv/real/browser?sort=rank&page=1
  */
 const pages = {
-  2021: 15,
-  2020: 35,
+  2021: 18,
+  2020: 34,
   anime: 253,
   book: 166,
   book2021: 43,
@@ -80,20 +80,20 @@ const pages = {
   write('./ids/anime-2021.json', data)
   data = []
 
-  /**
-   * anime 2020
-   */
-  for (let page = 1; page <= pages[2020]; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/anime/browser/airtime/2020?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/anime/browser/airtime/2020?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/anime-2020.json', data)
-  data = []
+  // /**
+  //  * anime 2020
+  //  */
+  // for (let page = 1; page <= pages[2020]; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/anime/browser/airtime/2020?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/anime/browser/airtime/2020?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/anime-2020.json', data)
+  // data = []
 
   /**
    * bangumi-data
@@ -133,35 +133,35 @@ const pages = {
   // write('./ids/book-rank.json', data)
   // data = []
 
-  /**
-   * book 2021
-   */
-  for (let page = 1; page <= pages.book2021; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/book/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/book/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/book-2021.json', data)
-  data = []
+  // /**
+  //  * book 2021
+  //  */
+  // for (let page = 1; page <= pages.book2021; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/book/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/book/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/book-2021.json', data)
+  // data = []
 
-  /**
-   * book 2020
-   */
-  for (let page = 1; page <= pages.book2020; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/book/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/book/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/book-2020.json', data)
-  data = []
+  // /**
+  //  * book 2020
+  //  */
+  // for (let page = 1; page <= pages.book2020; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/book/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/book/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/book-2020.json', data)
+  // data = []
 
   // /**
   //  * music rank
@@ -176,35 +176,35 @@ const pages = {
   // write('./ids/music-rank.json', data)
   // data = []
 
-  /**
-   * music 2021
-   */
-  for (let page = 1; page <= pages.music2021; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/music/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/music/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/music-2021.json', data)
-  data = []
+  // /**
+  //  * music 2021
+  //  */
+  // for (let page = 1; page <= pages.music2021; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/music/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/music/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/music-2021.json', data)
+  // data = []
 
-  /**
-   * music 2020
-   */
-  for (let page = 1; page <= pages.music2020; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/music/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/music/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/music-2020.json', data)
-  data = []
+  // /**
+  //  * music 2020
+  //  */
+  // for (let page = 1; page <= pages.music2020; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/music/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/music/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/music-2020.json', data)
+  // data = []
 
   // /**
   //  * game rank
@@ -219,35 +219,35 @@ const pages = {
   // write('./ids/game-rank.json', data)
   // data = []
 
-  /**
-   * game 2021
-   */
-  for (let page = 1; page <= pages.game2021; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/game/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/game/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/game-2021.json', data)
-  data = []
+  // /**
+  //  * game 2021
+  //  */
+  // for (let page = 1; page <= pages.game2021; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/game/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/game/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/game-2021.json', data)
+  // data = []
 
-  /**
-   * game 2020
-   */
-  for (let page = 1; page <= pages.game2020; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/game/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/game/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/game-2020.json', data)
-  data = []
+  // /**
+  //  * game 2020
+  //  */
+  // for (let page = 1; page <= pages.game2020; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/game/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/game/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/game-2020.json', data)
+  // data = []
 
   // /**
   //  * real rank page 1-83
@@ -262,35 +262,35 @@ const pages = {
   // write('./ids/real-rank.json', data)
   // data = []
 
-  /**
-   * real 2021
-   */
-  for (let page = 1; page <= pages.real2021; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/real/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/real/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/real-2021.json', data)
-  data = []
+  // /**
+  //  * real 2021
+  //  */
+  // for (let page = 1; page <= pages.real2021; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/real/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/real/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/real-2021.json', data)
+  // data = []
 
-  /**
-   * real 2020
-   */
-  for (let page = 1; page <= pages.real2020; page++) {
-    console.log(
-      `- fetching ${`https://bgm.tv/real/browser/airtime/2021?page=${page}`}`
-    )
-    const { data: indexHTML } = await fetch({
-      url: `https://bgm.tv/real/browser/airtime/2021?page=${page}`,
-    })
-    data.push(...cheerio.cheerioIds(indexHTML))
-  }
-  write('./ids/real-2020.json', data)
-  data = []
+  // /**
+  //  * real 2020
+  //  */
+  // for (let page = 1; page <= pages.real2020; page++) {
+  //   console.log(
+  //     `- fetching ${`https://bgm.tv/real/browser/airtime/2021?page=${page}`}`
+  //   )
+  //   const { data: indexHTML } = await fetch({
+  //     url: `https://bgm.tv/real/browser/airtime/2021?page=${page}`,
+  //   })
+  //   data.push(...cheerio.cheerioIds(indexHTML))
+  // }
+  // write('./ids/real-2020.json', data)
+  // data = []
 
   // /**
   //  * agefans
